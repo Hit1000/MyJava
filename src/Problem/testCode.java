2979b10da1ce26d2b0c445f0b4bb6f30c896cccc
Hit1000 <hiteshsingla209@gmail.com>
2024-02-17 01:13:46 +0530

    changes

--- src/Problem/testCode.java
+++ src/Problem/testCode.java
@@ -2,7 +2,7 @@ package Problem;
 
 public class testCode {
 //    https://tests.mettl.com/v2/authenticateKey/8e68e9a4
-    //            1557c062
-    //            8e68e9a4
-    //            cbe4c4da
+    //            1557c062  encoded array
+    //            8e68e9a4  decreasing array
+    //            cbe4c4da  max repeat num in 2d array
 }

366dc69ad18310774ef1da85ddde851e49d0e286
Hit1000 <hiteshsingla209@gmail.com>
2024-02-19 14:48:28 +0530

    changes

---
+++ src/Basic/expectionHandling.java
@@ -0,0 +1,80 @@
+package Basic;
+
+public class expectionHandling {
+
+//    run time error in known as expection
+//    ex - divide by 0 is run time error
+//    types of java exection
+//            1 check
+//            2 unchecked
+//
+//    unchecked exection - error only checked at run time
+//            ArithmeticException int a= 30/0;
+//            null pointer exception String s = null;sout(s.length());
+//            number format exception String s="abc";int a =Integer.parseInt(s);
+//            ArrayIndexOutOfBounds exception int[] a = new int[1];sout(a[3]);
+//            stringIndexoutOfBounds exception String s = new String("abc");sout(s.charAt(3));
+
+//    all classes related to exception is present in java.lang package
+//    throw able is a base class in all java
+//        vircual machine error
+//        out of memory error
+//        stack overflow error
+
+//    checked exception
+//            i/o exception
+//            sql exception
+//            class not found
+
+//    runtime exception / unchecked
+//            ArithmeticException int a= 30/0;
+//            null pointer exception String s = null;sout(s.length());
+//            number format exception String s="abc";int a =Integer.parseInt(s);
+//            ArrayIndexOutOfBounds exception int[] a = new int[1];sout(a[3]);
+//            stringIndexoutOfBounds exception String s = new String("abc");sout(s.charAt(3));
+
+//    to handle exception in java we have 5 keyword try, catch, throw, throws, finally
+//    try is not alone it is with catch or finally
+
+
+
+
+    public static void main(String[] args) {
+//        int a= 30/0;
+//        System.out.println(a);
+
+//        String s = null;
+//        System.out.println(s.length());
+
+//        String s="abc";int a =Integer.parseInt(s);
+
+//        int[] a = new int[1];
+//        System.out.println(a[3]);
+
+//        String s = new String("abc");
+//        System.out.println(s.charAt(3));
+
+//        Exception handling
+
+        System.out.println("h");
+        try {
+//            int a = 3/0;
+//            System.out.println(a);
+
+//            String s = null;
+//            System.out.println(s.length());
+
+            String s="abc";int a =Integer.parseInt(s);
+        }
+//        catch (ArithmeticException e){
+//            System.out.println("exception statement "+e);
+//        }
+//        catch (NullPointerException f){
+//            System.out.println("Null wala");
+//        }
+        catch (NumberFormatException e){
+            System.out.println("number ");
+        }
+        System.out.println("hello");
+    }
+}
--- src/Basic/string.java
+++ src/Basic/string.java
@@ -2,12 +2,28 @@ package Basic;
 
 public class string {
     public String toString(){
-        String x="ldkf";
+        String x="ldf";
         return x;
     }
-    public static void main(String c[])
+    public static void main(String[] args)
     {
 //        String gh = String.toString();
 //        System.out.println(gh);
+
+    String a = "hit";
+    String b = "hit";
+    String c = new String("hit");
+    String d = new String("hit");
+
+        //comparison of string
+//  1 == comparator it check reference and value
+//  2 .equals() method it check only values of string
+
+
+
+        System.out.println(a == b);
+        System.out.println(c.equals(d));
+        System.out.println(c.compareTo(d));
+
     }
 }

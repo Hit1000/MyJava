6968b4a0a0d4e30cdf97200a71f4034b2191688a
Hit1000 <hiteshsingla209@gmail.com>
2024-02-29 00:07:29 +0530

    changes

---
+++ src/Basic/staticExample/staticBlock.java
@@ -0,0 +1,24 @@
+package Basic.staticExample;
+
+public class staticBlock {
+
+    static int a = 2;
+    static int b;
+
+//    will only run once when the class is loaded first
+    static {
+        System.out.println("this is static block");
+        b = a*5;
+    }
+
+    public static void main(String[] args) {
+        staticBlock obj = new staticBlock();
+        System.out.println(a + " " + staticBlock.b);
+
+        b+=3;
+
+        staticBlock obj1 = new staticBlock();
+        System.out.println(b);
+
+    }
+}

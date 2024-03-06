bfa8219cdc5ce6770e750d772988840bbd929dd8
Hit1000 <hiteshsingla209@gmail.com>
2024-03-06 11:38:01 +0530

    changes

--- src/Problem/testCode.java
+++ src/Problem/testCode.java
@@ -2,10 +2,18 @@ package Problem;
 
 public class testCode {
 //    https://tests.mettl.com/v2/authenticateKey/8e68e9a4
-    //            1557c062  encoded array
-    //            8e68e9a4  decreasing array
-    //            cbe4c4da  max repeat num in 2d array
-    //            d2272e10  GenerateSeriesFindNth ap series
-//                814effc3  AlternateAddSub
-//                5106dfd   FindPasswordStableUnstable
+
+//    1557c062  encoded array
+//    8e68e9a4  decreasing array
+//    cbe4c4da  max repeat num in 2d array
+//    d2272e10  GenerateSeriesFindNth ap series
+//    814effc3  AlternateAddSub
+//    5106dfd   FindPasswordStableUnstable
+//    75c9faf0  GetCodeThroughStrings
+//    2f174a80  AdditionUsingStrings
+//    13486c16  IdentifyPossibleWords
+//    f05028d5  EncodingThreeStrings
+//    e4df74e5  FindStringCode
+
+
 }
